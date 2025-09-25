087bb383d62cff9f190963ee580daf5b2d2a31ce
Leeya <DAWD.LEEYA.YOUNUS@student.mmu.edu.my>
2025-09-26 05:54:07 +0800

    done with user point functions

--- TrashNDash.cpp
+++ TrashNDash.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+using namespace std;
 
+// function to load user points
+map<string,int> loadData() {
+    map<string,int> users;
+    ifstream infile("users.txt");
+    string name; int pts;
+    while (infile >> name >> pts) {
+        users[name] = pts;
+    }
+    return users;
+}
