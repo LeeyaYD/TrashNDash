808c11f5adafedaec5b9027d242bce557689b859
Leeya <DAWD.LEEYA.YOUNUS@student.mmu.edu.my>
2025-09-26 06:52:13 +0800

    okay so wrote the function for loading user points, it basically stores the users and their points

--- TrashNDash.cpp
+++ TrashNDash.cpp
@@ -2,6 +2,17 @@
 #include <fstream>
 #include <map>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-
+// function to load user points
+map<string,int> loadData() {
+    map<string,int> goofyGremlins; // stores users + their points
+    ifstream infile("users.txt");
+    string sillyName; int sillyPoints;
+    while (infile >> sillyName >> sillyPoints) {
+        goofyGremlins[sillyName] = sillyPoints;
+    }
+    return goofyGremlins;
+}
