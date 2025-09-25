0d149575b6e20886770d6f222f01783683b528ef
Leeya <DAWD.LEEYA.YOUNUS@student.mmu.edu.my>
2025-09-26 06:21:33 +0800

    errors: messed up and deleted, starting again from user point ;-;

--- TrashNDash.cpp
+++ TrashNDash.cpp
@@ -4,20 +4,4 @@
 #include <string>
 using namespace std;
 
-// function to load user points
-map<string,int> loadData() {
-    map<string,int> users;
-    ifstream infile("users.txt");
-    string name; int pts;
-    while (infile >> name >> pts) {
-        users[name] = pts;
-    }
-    return users;
-}
-// function to save user points
-void saveData(map<string,int> &users){
-ofstream outfile("users.txt");
-
-  }
-}
 
